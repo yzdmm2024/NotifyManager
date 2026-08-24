c61ec79637876ad12fd38b793147956aba21f164
yzdmm2024 <yzdmm2023@gmail.com>
2026-08-24 21:01:16 +0800

    fix: use NSNotification for init, fix button tap not working

--- CarCheckPlugin/Source/CarCheckFloatingView.m
+++ CarCheckPlugin/Source/CarCheckFloatingView.m
@@ -73,7 +73,14 @@ static CarCheckFloatingView *sShared = nil;
     [_floatBtn setTitle:@"🚗" forState:UIControlStateNormal];
     _floatBtn.titleLabel.font = [UIFont systemFontOfSize:20];
     [_floatBtn addTarget:self action:@selector(floatBtnTapped) forControlEvents:UIControlEventTouchUpInside];
-    [_floatBtn addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleFloatPan:)]];
+    // 用 tap 手势确保点击优先，pan 不 cancel 触摸
+    UITapGestureRecognizer *tapBtn = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(floatBtnTapped)];
+    [_floatBtn addGestureRecognizer:tapBtn];
+    UIPanGestureRecognizer *panGR = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleFloatPan:)];
+    panGR.cancelsTouchesInView = NO;
+    panGR.delaysTouchesBegan = NO;
+    panGR.delaysTouchesEnded = NO;
+    [_floatBtn addGestureRecognizer:panGR];
     // 动画：2秒后变半透明，点击时恢复不透明
     [self performSelector:@selector(fadeFloatBtn) withObject:nil afterDelay:2.0];
     [self addSubview:_floatBtn];

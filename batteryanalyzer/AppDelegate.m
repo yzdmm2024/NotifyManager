ecd6971fded903dd8d7743b84992a6e866201fbf
yzdmm2024 <yzdmm2023@gmail.com>
2026-08-25 11:13:51 +0800

    feat: battery analyzer TIPA app (Powerlog reader, no-sandbox); batteryanalyzer/AppDelegate.m

---
+++ batteryanalyzer/AppDelegate.m
@@ -0,0 +1,14 @@
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[ViewController new]];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end

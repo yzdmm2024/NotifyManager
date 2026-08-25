f3a392a18db3fc6f0c500488132cb977d9e233f8
yzdmm2024 <yzdmm2023@gmail.com>
2026-08-25 11:13:58 +0800

    feat: battery analyzer TIPA app (Powerlog reader, no-sandbox); batteryanalyzer/BatteryData.h

---
+++ batteryanalyzer/BatteryData.h
@@ -0,0 +1,24 @@
+#import <Foundation/Foundation.h>
+
+@interface AppUsage : NSObject
+@property (nonatomic, copy) NSString *bundleId;
+@property (nonatomic, copy) NSString *appName;
+@property (nonatomic) double screenOnTime;    // 秒
+@property (nonatomic) double backgroundTime;  // 秒
+@end
+
+@interface HourlyUsage : NSObject
+@property (nonatomic, copy) NSString *label;  // 如 "08-25 14:00"
+@property (nonatomic) NSInteger appCount;
+@end
+
+@interface BatteryData : NSObject
+@property (nonatomic) NSInteger currentLevel;      // 当前电量 %
+@property (nonatomic, strong) NSDate *lastChargeEnd; // 上次充电结束时间
+@property (nonatomic) NSInteger chargeEndLevel;    // 上次充电结束时的电量 %
+@property (nonatomic, strong) NSArray<AppUsage *> *appUsages;
+@property (nonatomic, strong) NSArray<HourlyUsage *> *hourlyUsages;
+@property (nonatomic, copy) NSString *errorMessage;
++ (instancetype)shared;
+- (void)loadData;
+@end
